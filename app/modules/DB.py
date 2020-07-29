f1d7f29fa1a47da207b8ea5a46d3869b4d433830
Justin Wang <justin.wang@charter.com>
2020-07-28 22:34:23 -0400

    pymysql module replacement

--- app/modules/DB.py
+++ app/modules/DB.py
@@ -1,24 +1,27 @@
 import csv
 import pymysql
-pymysql.install_as_MySQLdb()
 
 class Database(object):
+
     def _init_(self, host, password, user):
-        self.mydb = MySQLdb.connect(host='localhost',
+        self.db = pymysql.connect(host='localhost',
             user='root',
             passwd='',
             db='mydb')
     
     def update(self, file):
-        cursor = self.mydb.cursor()
-        csv_data = csv.reader(file('students.csv'))
+        cursor = self.db.cursor()
+        # csv_data = csv.reader(open('test.csv'))
+        csv_data = csv.reader(file('assets/schedules/schedule3.csv'))
+        # next(csv_data)
         for row in csv_data:
+            cursor.execute('INSERT INTO PM(col1,col2) VALUES(%s, %s)',row)
 
-            cursor.execute('INSERT INTO testcsv(names, \
-                classes, mark )' \
-                'VALUES("%s", "%s", "%s")', 
-                row)
+            # cursor.execute('INSERT INTO testcsv(names, \
+            #     classes, mark )' \
+            #     'VALUES("%s", "%s", "%s")', 
+            #     row)
         #close the connection to the database.
-        self.mydb.commit()
+        self.db.commit()
         cursor.close()
         print("Done")
